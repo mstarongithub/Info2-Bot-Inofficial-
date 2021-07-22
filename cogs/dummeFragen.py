1bb822d96a57c923e3b7743f749c0ee077050c20
MrEvil <evil.glurak@gmail.com>
2021-07-22 15:46:08 +0200

    Now uses regex to search for terms Still uses static strings as output Add ready notifier

--- cogs/dummeFragen.py
+++ cogs/dummeFragen.py
@@ -3,15 +3,19 @@ Automatic reaction to stupid phrases like if-loop
 """
 
 from discord.ext import commands
+import re
 
 class example(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f"{__name__} ready")
 
     """
     Format:
-        "keyInTest": "Answer"
+        ("key, be it regex or not", "Answer")
     """
     phrases = {
         "if-loop": "While it sometimes may look like it, if constructs are, in fact, not able to construct a loop on their own"
@@ -21,9 +25,12 @@ class example(commands.Cog):
     async def on_message(self, message):
         if message.author.id == self.bot.user.id:
             return
+        final = ""
         for i in self.phrases:
-            if i in message.content:
-                await message.reply(self.phrases[i])
+            if re.search(i, message.content):
+                final += f"{self.phrases[i]}\n"
+        if len(final) > 0:
+            await message.reply(final[:-1])
 
 def setup(bot):
     bot.add_cog(example(bot))
