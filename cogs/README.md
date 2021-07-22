044a1a70ff27e888dec5bee99063f024598cbe59
MrEvil <evil.glurak@gmail.com>
2021-07-22 09:16:20 +0200

    - update token names in authData template - simple readme for cogs folder added - main should now be functional in of itself

---
+++ cogs/README.md
@@ -0,0 +1,1 @@
+The bot's cogs are stored in here
--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-import discord
+from discord.ext import commands
 
 import authData
+
+"""
+This module's function is to start up the barebones bot
+and load in cogs from a subfolder
+"""
+
+bot = commands.Bot(command_prefix="i!")
+
+@bot.command()
+async def ping(context):
+    await context.send("Pong")
+
+
+if __name__ == '__main__':
+    bot.run(authData.BOT_TOKEN)
