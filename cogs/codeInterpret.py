51b494e402e7e2a02fb741d73b7b19092214dceb
beimg <beimgraben8@gmail.com>
2021-07-22 14:23:36 +0200

    Implement ExecCode

--- cogs/codeInterpret.py
+++ cogs/codeInterpret.py
@@ -3,6 +3,7 @@ Features regarding code interpretation and execution
 """
 
 from discord.ext import commands
+from discord.ext.commands.core import command
 
 from pistonapi import PistonAPI
 
@@ -11,7 +12,11 @@ class SyntaxCheck(commands.Cog):
     Check the syntax of code blocks
     """
 
-    def __init__(self, bot):
+    def __init__(self, bot, piston_api):
+        self.langs = [
+                key for key in piston_api.languages
+            ]
+        self.api = piston_api
         self.bot = bot
 
 class ExecCode(commands.Cog):
@@ -19,9 +24,34 @@ class ExecCode(commands.Cog):
     Execute code and display it´s output
     """
 
-    def __init__(self, bot):
+    def __init__(self, bot, piston_api):
+        langs = piston_api.languages
+        self.vers  = {
+                key : langs[key]['version'] for key in langs
+            }
+        self.langs = [
+                key for key in langs
+            ]
+        self.api = piston_api
         self.bot = bot
 
+    @commands.Cog.listener("on_message")
+    async def execute(self, context):
+        message = context.content
+        if context.content[:3] == "```":
+            lang = message.split('\n')[0][3:]
+            code = message.replace(f'```{lang}\n', '')[:-3]
+            if lang in self.langs:
+                await context.channel.send(
+                    'Output:```\n' +
+                    self.api.execute(
+                        language=lang, version=self.vers[lang], code=code) +
+                    '```'
+                )
+
+
 def setup(bot):
-    bot.add_cog(SyntaxCheck(bot))
-    bot.add_cog(ExecCode(bot))
+    piston = PistonAPI()
+
+    bot.add_cog(SyntaxCheck(bot, piston))
+    bot.add_cog(ExecCode(bot, piston))
