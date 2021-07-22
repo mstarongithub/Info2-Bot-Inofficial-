3c0311ce35b8a5dc56d745c6c70235aa50314711
beimg <beimgraben8@gmail.com>
2021-07-22 11:33:33 +0200

    Add SyntaxCheck Cog

--- cogs/code_interpret.py
+++ cogs/code_interpret.py
@@ -1,3 +1,11 @@
+from discord.ext import commands
+
 """
 Features regarding code interpretation and execution
 """
+
+class SyntaxCheck(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    
